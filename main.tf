731b775d3497089be4171bb2ad0bc11d4899f357
chiranjeevi keerthi <chiranjeevikeerthi77777@gmail.com>
2025-04-16 16:55:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,10 +4,10 @@ region = "ap-south-1"
 
 resource "aws_instance" "one" {
 count = 4
-ami = "ami-07a6f770277670015"
+ami = "ami-080b1a55a0ad28c02"
 instance_type = "t2.medium"
 key_name = "netflix"
-vpc_security_group_ids = ["sg-03a9548c406b8d450"]
+vpc_security_group_ids = ["sg-0473d216958d0f177"]
 tags = {
 Name = var.instance_names[count.index]
 }
